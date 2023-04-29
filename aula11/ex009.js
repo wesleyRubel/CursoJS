ee1af54dbc5bf41fa0592f37fb358ec545e9e229
Wesley Kozach Rubel <wkozach@gmail.com>
2023-04-29 11:00:23 -0300

    Create ex009.js

---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais='EUA';
+console.log(`Vivendo em ${pais}`);
+if(pais=='Brasil'){ //Condição composta
+    console.log('Você é brasileiro');
+}else{
+    console.log('Você é estrangeiro');
+}
