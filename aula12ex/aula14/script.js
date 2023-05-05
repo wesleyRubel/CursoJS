23553b4e7ff36251309568d7fce7b7b7199c12f7
Wesley Kozach Rubel <wkozach@gmail.com>
2023-05-05 10:41:42 -0300

    Update script.js

--- aula12ex/aula14/script.js
+++ aula12ex/aula14/script.js
@@ -3,6 +3,21 @@ function carregar(){
     var img=window.document.getElementById('imagem');
     var data=new Date();
     var hora=data.getHours();
+    //var hora=18;
     var min=data.getMinutes();
     msg.innerHTML=`Agora são ${hora} horas e ${min} minutos`;
+    if(hora>=0 && hora<12){
+        //Bom dia
+        img.src='imagens/manha.png';
+        //window.document.getElementsByTagName('body')[0].style.backgroundColor="#BEB58A";
+        document.body.style.backgroundColor="#BEB58A";
+    }else if(hora>=12 && hora<18){
+        //Boa tarde
+        img.src='imagens/tarde.png';
+        window.document.body.style.backgroundColor="#8E5937";
+    }else{
+        //Boa noite
+        img.src='imagens/noite.png';
+        window.document.body.style.backgroundColor="#342741";
+    }
 }
