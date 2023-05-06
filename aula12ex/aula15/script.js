29c99b01373161677a2cf444905adb55d3a81830
Wesley Kozach Rubel <wkozach@gmail.com>
2023-05-06 12:40:12 -0300

    Update script.js

--- aula12ex/aula15/script.js
+++ aula12ex/aula15/script.js
@@ -3,7 +3,7 @@ function verificar(){
     var ano=data.getFullYear();
     var fAno=window.document.getElementById('inAno');
     var res=window.document.querySelector('div#outMsg');
-    var img=window.document.getElementById('foto');
+    var foto=window.document.getElementById('foto');
     if(fAno.value.length==0 || fAno.value<1900 || fAno.value>ano){
         window.alert('[ERRO] Verifique os dados e tente novamente');
         window.document.getElementById('anoNasc').style.borderWidth="0px 0px 1px 0px";
@@ -14,31 +14,54 @@ function verificar(){
         var idade=ano-Number(fAno.value);
         //res.innerHTML=`Idade calculada = ${idade}`;
         var genero='';
-       /* var img=window.document.createElement('img'); //Criando um elemento HTML
-        img.setAttribute('id','foto'); //Criando um id para o elemento img*/
+        var img=window.document.createElement('img'); //Criando um elemento HTML
+        img.setAttribute('id','imagem'); //Criando um id para o elemento img
         if(fsex[0].checked){
             genero='Homem';
             if(idade>=0 && idade<=10){
                 //Criança
                 res.innerHTML=`Detectamos um ${genero} de ${idade} anos`;
-                img.innerHTML='<img src="imagens/homemCrianca.png" alt="Menino">';
+                img.setAttribute('src','imagens/homemCrianca.png');
+                img.setAttribute('alt','Menino');
             }else if(idade<21){
                 //Jovem
+                res.innerHTML=`Detectamos um ${genero} de ${idade} anos`;
+                img.setAttribute('src','imagens/homemJovem.png');
+                img.setAttribute('alt','Homem jovem');
             }else if(idade<50){
                 //Adulto
+                res.innerHTML=`Detectamos um ${genero} de ${idade} anos`;
+                img.setAttribute('src','imagens/homemAdulto.png');
+                img.setAttribute('alt','Homem adulto');
             }else{
                 //Idoso
+                res.innerHTML=`Detectamos um ${genero} de ${idade} anos`;
+                img.setAttribute('src','imagens/homemIdoso.png');
+                img.setAttribute('alt','Homem idoso');
             }
         }else if(fsex[1].checked){
             genero='Mulher';
             if(idade>=0 && idade<=10){
                 //Criança
+                res.innerHTML=`Detectamos uma ${genero} de ${idade} anos`;
+                img.setAttribute('src','imagens/mulherCrianca.png');
+                img.setAttribute('alt','Menina');
             }else if(idade<21){
                 //Jovem
+                res.innerHTML=`Detectamos uma ${genero} de ${idade} anos`;
+                img.setAttribute('src','imagens/mulherJovem.png');
+                img.setAttribute('alt','Mulher jovem');
+                img.style.width="400px";
             }else if(idade<50){
                 //Adulto
+                res.innerHTML=`Detectamos uma ${genero} de ${idade} anos`;
+                img.setAttribute('src','imagens/mulherAdulta.png');
+                img.setAttribute('alt','Mulher adulta');
             }else{
                 //Idoso
+                res.innerHTML=`Detectamos uma ${genero} de ${idade} anos`;
+                img.setAttribute('src','imagens/mulherIdosa.png');
+                img.setAttribute('alt','Mulher idosa');
             }
         }else{
             window.alert('[ERRO] Verifique os dados e tente novamente');
@@ -46,6 +69,7 @@ function verificar(){
             window.document.getElementById('outSexo').style.borderStyle="dotted";
             window.document.getElementById('outSexo').style.borderColor="red";    
         }
+        foto.appendChild(img); //Incluindo o elemento img a div de id="foto"
     }
 }
 
