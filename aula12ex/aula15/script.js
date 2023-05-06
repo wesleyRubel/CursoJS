ecec73f35f6c036c1f923e14ad0600940af790bc
Wesley Kozach Rubel <wkozach@gmail.com>
2023-05-06 10:17:33 -0300

    Update script.js

---
+++ aula12ex/aula15/script.js
@@ -0,0 +1,6 @@
+function verificar(){
+    var data=new Date();
+    var ano=data.getFullYear();
+    var fAno=window.document.getElementById('inAno');
+    var res=window.document.querySelector('div#outMsg');
+}
