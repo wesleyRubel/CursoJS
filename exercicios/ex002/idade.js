2c87169b24623e6c079c61b87b30035d579868a5
Wesley Kozach Rubel <wkozach@gmail.com>
2023-05-07 13:53:29 -0300

    Atualizei ex002

--- exercicios/ex002/idade.js
+++ exercicios/ex002/idade.js
@@ -1,74 +1,71 @@
-var agora = new Date();
-var anoAtual = agora.getFullYear();
-function verificaIdade() {
-    //Selecionando os elementos HTML
-    var anoNascimento = window.document.getElementById('inAno');
-    var masculino = window.document.getElementsByName('sexo')[0];
-    var feminino = window.document.getElementsByName('sexo')[1];
-    //Recebendo os valores dos elementos
-    var anoNasc = Number(anoNascimento.value);
-    var idade = anoAtual - anoNasc;
-    //Verificando se o usuário preencheu os campos corretamente
-    if (anoNasc != '' && anoNasc < anoAtual) {
-        if (masculino.checked) {
-            if (idade < 18) {
-                window.document.getElementById('outMsg').innerHTML = `Garoto de ${idade} anos`;
-                window.document.getElementById('outMsg').style.textAlign = "center";
-                window.document.getElementById('outMsg').style.fontWeight = "bold";
-                window.document.getElementById('outImage').style.display = "block";
-                window.document.getElementById('outImage').style.background = "url(./imagens/homemCriança-300.jpg) center center no-repeat";
-                window.document.getElementById('outImage').style.backgroundSize = "cover";
-            } else if (idade < 65) {
-                window.document.getElementById('outMsg').innerHTML = `Homem de ${idade} anos`;
-                window.document.getElementById('outMsg').style.textAlign = "center";
-                window.document.getElementById('outMsg').style.fontWeight = "bold";
-                window.document.getElementById('outImage').style.display = "block";
-                window.document.getElementById('outImage').style.background = "url(./imagens/homemJovem-200.jpg) center center no-repeat";
-                window.document.getElementById('outImage').style.backgroundSize = "cover";
-            } else {
-                window.document.getElementById('outMsg').innerHTML = `Homem de ${idade} anos`;
-                window.document.getElementById('outMsg').style.textAlign = "center";
-                window.document.getElementById('outMsg').style.fontWeight = "bold";
-                window.document.getElementById('outImage').style.display = "block";
-                window.document.getElementById('outImage').style.background = "url(./imagens/homemIdoso-200.jpeg) center center no-repeat";
-                window.document.getElementById('outImage').style.backgroundSize = "cover";
-            }
-        } else if (feminino.checked) {
-            if (idade < 18) {
-                window.document.getElementById('outMsg').innerHTML = `Garota de ${idade} anos`;
-                window.document.getElementById('outMsg').style.textAlign = "center";
-                window.document.getElementById('outMsg').style.fontWeight = "bold";
-                window.document.getElementById('outImage').style.display = "block";
-                window.document.getElementById('outImage').style.background = "url(./imagens/mulherCriança-300.jpg) center center no-repeat";
-                window.document.getElementById('outImage').style.backgroundSize = "cover";
-            } else if (idade < 65) {
-                window.document.getElementById('outMsg').innerHTML = `Mulher de ${idade} anos`;
-                window.document.getElementById('outMsg').style.textAlign = "center";
-                window.document.getElementById('outMsg').style.fontWeight = "bold";
-                window.document.getElementById('outImage').style.display = "block";
-                window.document.getElementById('outImage').style.background = "url(./imagens/mulherJovem-200.jpg) center center no-repeat";
-                window.document.getElementById('outImage').style.backgroundSize = "cover";
-            } else {
-                window.document.getElementById('outMsg').innerHTML = `Mulher de ${idade} anos`;
-                window.document.getElementById('outMsg').style.textAlign = "center";
-                window.document.getElementById('outMsg').style.fontWeight = "bold";
-                window.document.getElementById('outImage').style.display = "block";
-                window.document.getElementById('outImage').style.background = "url(./imagens/mulherIdosa-200.jpg) center center no-repeat";
-                window.document.getElementById('outImage').style.backgroundSize = "cover";
-            }
-        } else {
-            alert('ERRO! Preencha corretamente os campos');
-            window.document.getElementById('inputsS').style.borderWidth = "0px 0px 1px 0px";
-            window.document.getElementById('inputsS').style.borderColor = "red";
-            window.document.getElementById('inputsS').style.borderStyle = "dotted";
-        }
+function verificar() {
+    var data = new Date();
+    var anoA = data.getFullYear();
+    var anoN = window.document.getElementById('inAno');
+    if (anoN.value.length == 0 || anoN.value < 1900 || anoN.value > anoA) {
+        window.alert('[ERRO] Preencha os campos corretamente');
+        window.document.getElementById('labelAno').style.borderWidth = "0px 0px 1px 0px";
+        window.document.getElementById('labelAno').style.borderStyle = "dotted";
+        window.document.getElementById('labelAno').style.borderColor = "red";
     } else {
-        alert('ERRO! Preencha corretamente os campos');
-        window.document.getElementById('inputsA').style.borderWidth = "0px 0px 1px 0px";
-        window.document.getElementById('inputsA').style.borderColor = "red";
-        window.document.getElementById('inputsA').style.borderStyle = "dotted";
+        var idade = anoA - Number(anoN.value);
+        var sex = window.document.getElementsByName('sexo');
+        var genero = '';
+        if (sex[0].checked) {
+            genero = 'Homem';
+            var img=document.createElement('img');
+           // img.setAttribute('src','imagens/homemAdulto.png');
+           // img.setAttribute('alt','Homem Adulto');
+            window.document.querySelector('p#outMsg').innerHTML = `Detectamos um ${genero} de ${idade} anos`;
+            if(idade>=0 && idade<=10){
+                //Criança
+                img.setAttribute('src','imagens/menino.png');
+                img.setAttribute('alt','Menino');
+            }else if(idade<21){
+                //Jovem
+                img.setAttribute('src','imagens/homemJovem.png');
+                img.setAttribute('alt','Homem Jovem');
+            }else if(idade<50){
+                //Adulto
+                img.setAttribute('src','imagens/homemAdulto.png');
+                img.setAttribute('alt','Homem Adulto');
+            }else{
+                //Idoso
+                img.setAttribute('src','imagens/homemIdoso.png');
+                img.setAttribute('alt','Homem Idoso');
+            }
+        window.document.getElementById('outMsg').style.textAlign = "center";
+        } else if (sex[1].checked) {
+            genero = 'Mulher';
+            var img=document.createElement('img');
+           // img.setAttribute('src','imagens/mulherAdulta.png');
+           // img.setAttribute('alt','Mulher Adulta');
+            window.document.querySelector('p#outMsg').innerHTML = `Detectamos uma ${genero} de ${idade} anos`;
+            if(idade>=0 && idade<=10){
+                //Criança
+                img.setAttribute('src','imagens/menina.png');
+                img.setAttribute('alt','Menina');
+            }else if(idade<21){
+                //Jovem
+                img.setAttribute('src','imagens/mulherJovem.png');
+                img.setAttribute('alt','Mulher Jovem');
+            }else if(idade<50){
+                //Adulto
+                img.setAttribute('src','imagens/mulherAdulta.png');
+                img.setAttribute('alt','Mulher Adulta');
+            }else{
+                //Idoso
+                img.setAttribute('src','imagens/mulherIdosa.png');
+                img.setAttribute('alt','Mulher Idosa');
+            }
+        window.document.getElementById('outMsg').style.textAlign = "center";
+        } else {
+            window.alert('[ERRO] Preencha os campos corretamente');
+            window.document.getElementById('spanSexo').style.borderWidth = "0px 0px 1px 0px";
+            window.document.getElementById('spanSexo').style.borderStyle = "dotted";
+            window.document.getElementById('spanSexo').style.borderColor = "red";
+        }
+        window.document.getElementById('outImage').appendChild(img);
+        window.document.getElementById('outImage').style.textAlign="center";
     }
 }
-
-var verificar = window.document.getElementById('btVerificar');
-verificar.addEventListener('click', verificaIdade);
