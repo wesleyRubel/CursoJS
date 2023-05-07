2fc8f6a32add46154d968ed47df36e8d79a0f423
Wesley Kozach Rubel <wkozach@gmail.com>
2023-05-07 10:39:01 -0300

    Atualizei ex001

--- exercicios/ex001/horario.js
+++ exercicios/ex001/horario.js
@@ -1,28 +1,53 @@
-var agora=new Date();
-var horas=agora.getHours();
-var minutos=agora.getMinutes();
+/*#F2BF5E - Manhã
+#D78040 - Tarde
+#362D4C - Noite
+#233eff - Padrão
+*/
 
-/*var horas=Number(prompt('Informe a hora atual'));*/
-
-if(horas>=0 && horas<=5){
-    window.document.getElementById('outBackground').style.backgroundColor="#142540";
-    window.document.querySelector('h3#outHora').innerHTML=`Agora são ${horas} horas e ${minutos} minutos`;
-    window.document.getElementById('outImage').style.background="url(./imagens/madrugada-200.jpg) center center no-repeat";
-    window.document.getElementById('outImage').style.backgroundSize="cover";        
-}
-else if(horas>=6 && horas<=12){
-    window.document.getElementById('outBackground').style.backgroundColor="#FFC45E";
-    window.document.querySelector('h3#outHora').innerHTML=`Agora são ${horas} horas e ${minutos} minutos`;
-    window.document.getElementById('outImage').style.background="url(./imagens/Amanhecer-200.jpg) center center no-repeat";
-    window.document.getElementById('outImage').style.backgroundSize="cover";
-}else if(horas>12 && horas<=18){
-    window.document.getElementById('outBackground').style.backgroundColor="#E86A51";
-    window.document.querySelector('h3#outHora').innerHTML=`Agora são ${horas} horas e ${minutos} minutos`;
-    window.document.getElementById('outImage').style.background="url(./imagens/entardecer-200.jpg) center center no-repeat";
-    window.document.getElementById('outImage').style.backgroundSize="cover";    
-}else{
-    window.document.getElementById('outBackground').style.backgroundColor="#142540";
-    window.document.querySelector('h3#outHora').innerHTML=`Agora são ${horas} horas e ${minutos} minutos`;
-    window.document.getElementById('outImage').style.background="url(./imagens/anoitecer-200.webp) center center no-repeat";
-    window.document.getElementById('outImage').style.backgroundSize="cover";       
+function carregar(){
+    var data=new Date();
+    var horas=data.getHours();
+   // var horas=20;
+    var min=data.getMinutes();
+    var outHoras=window.document.getElementById('outHora');
+    var outImage=window.document.getElementById('outImage');
+    outHoras.style.marginTop="10px";
+    outHoras.innerHTML=`Agora são ${horas} horas e ${min} minutos`;
+    if(horas>=0 && horas<12){
+        window.document.getElementsByTagName('body')[0].style.backgroundColor="#F2BF5E";
+        var bDia=document.createElement('p');
+        var foto=document.createElement('img');
+        foto.setAttribute('src','imagens/amanhecer.png');
+        foto.setAttribute('alt','Amanhecer');
+        bDia.innerHTML="Bom dia!";
+        bDia.style.textAlign="center";
+        bDia.style.fontWeight="bold";
+        bDia.style.marginBottom="10px";
+        window.document.getElementsByTagName('section')[0].appendChild(bDia);
+        outImage.appendChild(foto);
+    }else if(horas<19){
+        window.document.getElementsByTagName('body')[0].style.backgroundColor="#D78040";
+        var bDia=document.createElement('p');
+        var foto=document.createElement('img');
+        foto.setAttribute('src','imagens/entardecer.png');
+        foto.setAttribute('alt','Entardecer');
+        bDia.innerHTML="Boa Tarde!";
+        bDia.style.textAlign="center";
+        bDia.style.fontWeight="bold";
+        bDia.style.marginBottom="10px";
+        window.document.getElementsByTagName('section')[0].appendChild(bDia);
+        outImage.appendChild(foto);
+    }else{
+        window.document.getElementsByTagName('body')[0].style.backgroundColor="#362D4C";
+        var bDia=document.createElement('p');
+        var foto=document.createElement('img');
+        foto.setAttribute('src','imagens/anoitecer.png');
+        foto.setAttribute('alt','Anoitecer');
+        bDia.innerHTML="Boa Noite!";
+        bDia.style.textAlign="center";
+        bDia.style.fontWeight="bold";
+        bDia.style.marginBottom="10px";
+        window.document.getElementsByTagName('section')[0].appendChild(bDia);
+        outImage.appendChild(foto);    
+    }
 }
