6600fdfe53c9a223e5ef5a2cce18bb9192ae9b6c
Wesley Kozach Rubel <wkozach@gmail.com>
2023-05-09 14:55:09 -0300

    Create ex014.js

---
+++ aula13/ex014.js
@@ -0,0 +1,21 @@
+/*console.log('Olá! Tudo bem?');
+console.log('Olá! Tudo bem?');
+console.log('Olá! Tudo bem?');
+console.log('Olá! Tudo bem?');
+console.log('Olá! Tudo bem?');
+console.log('Olá! Tudo bem?');
+console.log('Olá! Tudo bem?');
+console.log('Olá! Tudo bem?');*/
+
+var num=1;
+/*while(num<=500){
+    console.log(`Passo ${num}`);
+    num++; //num+=1 num=num+1
+}*/
+
+do{
+    console.log(`Passo ${num}`);
+    num++
+}while(num<=0)
+
+console.log('Fim da repetição');
