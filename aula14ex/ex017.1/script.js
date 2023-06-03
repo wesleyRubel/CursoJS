21a683ad42ab6b01e8d346678df1a5305edab90e
Wesley Kozach Rubel <wkozach@gmail.com>
2023-06-03 15:32:56 -0300

    Exercício 017.1 concluído

--- aula14ex/ex017.1/script.js
+++ aula14ex/ex017.1/script.js
@@ -1,9 +1,22 @@
 function tabuada(){
     var num=document.getElementById('inNumero');
     var tab=document.getElementById('selTabuada');
-    if(num.value.lenght==0){
+    if(num.value.length == 0){
         window.alert('Por favor, digite um número!');
     }else{
         var n=Number(num.value);
+        var c=1;
+        tab.innerHTML=' ';
+        //document.getElementById('op').style.display="none";
+        while (c<=10) {
+            var item=document.createElement('option'); 
+            item.text=`${n} X ${c} = ${n*c}`;  
+            item.value=`tab${c}`; 
+            tab.appendChild(item);
+            c++;
+        }
     }
 }
+
+var bt=document.getElementById('btGerar');
+bt.addEventListener('click',tabuada);
