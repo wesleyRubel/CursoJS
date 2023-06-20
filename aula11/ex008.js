4ae891e834a6dd7e884a464e75ecb6227d3e2a6b
Wesley Kozach Rubel <wkozach@gmail.com>
2023-06-20 19:10:33 -0300

    Create ex008.js

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 75.5;
+console.log(`A velocidade de seu veículo é ${vel} Km/h`);
+if(vel > 60){
+    console.log(`MULTADO! Você excedeu o limite de velocidade`);    
+}
+console.log(`Dirija sempre usando cinto de segurança`);
