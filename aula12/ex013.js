2b91722fcb865d266c33ded74ed0ae82f26f156b
Wesley Kozach Rubel <wkozach@gmail.com>
2023-06-21 19:07:40 -0300

    Update ex013.js

--- aula12/ex013.js
+++ aula12/ex013.js
@@ -1,36 +1,39 @@
-var agora=new Date();
-var diaSem=agora.getDay();
+var agora = new Date();
+var dS = agora.getDay();
+
 /*
-    0 = Domingo
-    1 = Segunda
-    2 = Terça
-    3 = Quarta
-    4 = Quinta
-    5 = Sexta
-    6 = Sábado
+    0 - Domingo
+    1 - Segunda-Feira
+    2 - Terça-Feira
+    3 - Quarta-Feira
+    4 - Quinta-Feira
+    5 - Sexta-Feira
+    6 - Sábado
 */
 
-switch(diaSem){
+switch(dS){
     case 0:
-        console.log('Hoje é domingo');
+        console.log("Hoje é domingo");
         break;
     case 1:
-        console.log('Hoje é segunda-feira');
+        console.log("Hoje é segunda-feira");
         break;
     case 2:
-        console.log('Hoje é terça-feira');
+        console.log("Hoje é terça-feira");
         break;
     case 3:
-        console.log('Hoje é quarta-feira');
+        console.log("Hoje é quarta-feira");
         break;
     case 4:
-        console.log('Hoje é quinta-feira');
+        console.log("Hoje é quinta-feira");
         break;
     case 5:
-        console.log('Hoje é sexta-feira');
+        console.log("Hoje é sexta-feira");
         break;
     case 6:
-        console.log('Hoje é sábado');
+        console.log("Hoje é sábado");
+        break;
     default:
-        console.log('O valor informado não corresponde a nenhum dia da semana');
+        console.log("O valor não corresponde há nenhum dia da semana");
+        break;
 }
