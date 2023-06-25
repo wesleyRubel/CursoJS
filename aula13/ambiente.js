c6c6b103dd2bb73d7d0381ff3fb8e228de50db6f
Wesley Kozach Rubel <wkozach@gmail.com>
2023-06-25 11:07:55 -0300

    Update ambiente.js

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -7,13 +7,18 @@ console.log('Olá mundo!')
 console.log('Olá mundo!')
 console.log('Olá mundo!')*/
 
-var c=1
-/*while (c <= 500){
+/*var c=1
+while (c <= 500){
     console.log(`${c}º Passo`)
     c++
-}*/
+}
 
 do{
     console.log(`${7} X ${c} = ${7*c}`)
     c++
 }while(c < 11)
+*/
+const num = 10
+for(var c = 1;c <= 10;c++){
+    console.log(`${num} X ${c} = ${num*c}`)
+}
