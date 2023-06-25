857ed7d7f6672c27a79e809ef219421fea60c45c
Wesley Kozach Rubel <wkozach@gmail.com>
2023-06-25 10:40:43 -0300

    Atualizei exercício

---
+++ aula13/ambiente.js
@@ -0,0 +1,19 @@
+/*console.log('Olá mundo!')
+console.log('Olá mundo!')
+console.log('Olá mundo!')
+console.log('Olá mundo!')
+console.log('Olá mundo!')
+console.log('Olá mundo!')
+console.log('Olá mundo!')
+console.log('Olá mundo!')*/
+
+var c=1
+/*while (c <= 500){
+    console.log(`${c}º Passo`)
+    c++
+}*/
+
+do{
+    console.log(`${7} X ${c} = ${7*c}`)
+    c++
+}while(c < 11)
