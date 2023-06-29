382b03d7b2a379df1443c590eadc0388e65c9ad9
Wesley Kozach Rubel <wkozach@gmail.com>
2023-06-29 11:57:06 -0300

    Atualizei exercício

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,4 +1,4 @@
-function gerarTabuada(){
+/*function gerarTabuada(){
     var numero=window.document.getElementById('inNumero');
     if(numero.value==''){
         window.alert('Digite um número para gerar tabuada');
@@ -10,5 +10,30 @@ function gerarTabuada(){
         }
         window.document.getElementById('outTabuada').style.display="inline-block";
         window.document.getElementById('outTabuada').innerHTML=tabuada;
+    }
+}*/
+
+function gerarTabuada(){
+    let num = window.document.querySelector('input#inNumero')
+    let tab = window.document.querySelector('select#tab')
+    //Validando a entrada
+    if(num.value.length == 0){
+        window.alert('Digite um número para gerar tabuada')
+        num.focus()
+        tab.innerHTML = '<option>Digite um número acima</option>'
+    }else{
+        //window.alert('Tudo OK!')
+        tab.innerHTML = ''
+        let n = Number(num.value)
+        let c = 0
+        while(c <= 10){
+            let op = window.document.createElement('option')
+            op.setAttribute('value',`op${c}`)
+            tab.appendChild(op)
+            op.innerHTML = `${n} X ${c} = ${n*c}`
+            c++
+        }
+        num.value = ''
+        num.focus()
     }
 }
