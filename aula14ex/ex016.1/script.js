5ae75619d9be7424eb9a50361f695b93e0166b8d
Wesley Kozach Rubel <wkozach@gmail.com>
2023-06-29 10:52:22 -0300

    Atualizei exercício

--- aula14ex/ex016.1/script.js
+++ aula14ex/ex016.1/script.js
@@ -1,4 +1,4 @@
-function contar() {
+/*function contar() {
     var ini = window.document.querySelector('input#iInicio');
     var fim = window.document.querySelector('input#iFim');
     var passo = window.document.querySelector('input#iPasso');
@@ -32,4 +32,41 @@ function contar() {
             res.innerHTML = saida;
         }
     }
-}
+}*/
+
+function contar(){
+    //Criando os objetos referentes aos elementos a serem manipulados
+    var inicio = window.document.getElementById('inicio')
+    var fim = window.document.getElementById('fim')
+    var passo = window.document.getElementById('passo')
+    var res = window.document.getElementById('res')
+
+    //Validando os dados de entrada
+    if(inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        window.alert('[ERRO] Faltam dados!')
+        res.innerHTML = 'Impossível contar...'
+    }else{
+        //window.alert('Dados validados')
+        res.innerHTML = 'Contando:'
+        //Convertendo os valores de entrada
+        var i = Number(inicio.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
+        //Varificando o valor do passo
+        if(p <= 0){
+            window.alert('PASSO inválido! Considerando PASSO 1')
+            p = 1
+        }
+        //Contagem
+        if(i < f){ //Crescente
+            for(var c = i;c <= f;c += p){
+                res.innerHTML += ` ${c} \u{1f449}`
+            }
+        }else{ //Decrescente
+            for(var c = i;c >= f;c -= p){
+                res.innerHTML += ` ${c} \u{1f449}`
+            }
+        }
+        res.innerHTML += ` \u{1f3c1}`
+    }
+} 
