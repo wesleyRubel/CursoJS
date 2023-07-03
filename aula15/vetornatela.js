3ce2a89fe596678140c1c63f16e32095430a14fb
Wesley Kozach Rubel <wkozach@gmail.com>
2023-07-03 11:08:47 -0300

    Update vetornatela.js

--- aula15/vetornatela.js
+++ aula15/vetornatela.js
@@ -1,11 +1,17 @@
-let num=[2,5,3,7,9,8,1];
-num.sort();
-/*let pos=0;
-while(pos<num.length){
-    console.log(`O valor da posição ${pos} = ${num[pos]}`);
-    pos++;
+let valores = [8,1,7,4,2,9]
+/*let pos = 0
+while(pos < valores.length){
+    console.log(`Posição ${pos} = ${valores[pos]}`)
+    pos++
 }*/
+valores.sort()
+for(let pos in valores){
+    console.log(`Posição ${pos} = ${valores[pos]}`)
+}
 
-for(let pos in num){
-    console.log(`O valor da posição ${pos} = ${num[pos]}`);    
+let num = 6
+if(valores.indexOf(num) != -1){
+    console.log(`O valor existe dentro do array = ${valores[valores.indexOf(num)]}`)
+}else{
+    console.log(`Valor não foi encontrado dentro do vetor`)
 }
