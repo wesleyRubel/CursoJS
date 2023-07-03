af58475c582b279b657452b23a92e0ccb57d80fa
Wesley Kozach Rubel <wkozach@gmail.com>
2023-07-03 10:33:30 -0300

    Update ambiente.js

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,13 +1,13 @@
-let num=[2,5,3,7,9]; //Declarando um vetor de 5 elementos
-console.log(`Nosso vetor é composto pelos valores ${num}`);
-console.log(`O valor do índice 0 é ${num[0]}`);
-num[5]=6; //Cria um novo elemento de índice 5 ao vetor que irá receber o valor 6
-console.log(`Vetor atualizado ${num}`);
-num.push(8);
-console.log(`Vetor atualizado ${num}`);
-let n=num.length;
-console.log(`Nosso array tem ${n} elementos`);
-num.sort(); //Ordena os valores do vetor em ordem crescente
-console.log(`Vetor organizado ${num}`);
-let i=num.indexOf(1);
-console.log(i);
+let num = [5,8,2,9,3] //Declarando um array e atribuímos 5 elementos
+//0 = 5, 1 = 8, 2 = 2, 3 = 9, 4 = 3
+num[5] = 6 //O JavaScript ira criar um espaço no array com o índice 5 e atribuir o valor 10
+num.push(7) //Cria um elemento no final do array e guarda o valor 7
+console.log(`Nosso vetor é o ${num}`)
+console.log(`O array possui ${num.length} elementos`)
+for(let c = 0;c < num.length; c++){
+    console.log(`Posição ${c} = ${num[c]}`)
+}
+num.sort() //Ordena os elementos do vetor em ordem crescente
+for(let c = 0;c < num.length;c++){
+    console.log(`Posição ${c} = ${num[c]}`)
+}
