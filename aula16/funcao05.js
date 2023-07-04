052135cb0bfa8ed94aec2b359fe7bb544c4c0b9d
Wesley Kozach Rubel <wkozach@gmail.com>
2023-07-04 10:55:17 -0300

    Update funcao05.js

--- aula16/funcao05.js
+++ aula16/funcao05.js
@@ -1,8 +1,18 @@
-function fatorial(n){
+//Recursividade em funções
+
+/* 
+5! = 5x4x3x2x1
+5! = 5x4!
+n! = nx(n-1)!
+1! = 1
+*/
+
+function fatorial(n=0){
     if(n == 1){
-        return 1;
+        return 1
     }else{
-        return n*fatorial(n-1);
+        return n*fatorial(n-1)
     }
 }
-console.log(fatorial(7));
+
+console.log(`Fatorial = ${fatorial(8)}`)
