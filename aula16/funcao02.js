8087e56b071b27a589eb01d7df2fca6d786f2e15
Wesley Kozach Rubel <wkozach@gmail.com>
2023-07-04 10:22:04 -0300

    Update funcao02.js

--- aula16/funcao02.js
+++ aula16/funcao02.js
@@ -1,6 +1,5 @@
-function soma(n1=0,n2=0){
-    return n1+n2;
+function soma(n1 = 0,n2 = 0){
+    return n1 + n2
 }
 
-let res=soma(20,-40);
-console.log(`Soma = ${res}`);
+console.log(`Soma = ${soma(10,-15)}`)
