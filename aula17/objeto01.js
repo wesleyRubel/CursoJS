f231007b5337fc0e98186078aa855cd7e5f36f2a
Wesley Kozach Rubel <wkozach@gmail.com>
2023-07-06 09:31:18 -0300

    Update objeto01.js

--- aula17/objeto01.js
+++ aula17/objeto01.js
@@ -1,7 +1,16 @@
-let amigo={nome:'José',sexo:'M',peso:85.4,engordar(p=0){
-    console.log('Engordou');
-    this.peso+=p;
-}}; //Declarando um objeto
-console.log(`${amigo.nome} pesa ${amigo.peso} Kg`);
-amigo.engordar(2);
-console.log(`${amigo.nome} pesa agora ${amigo.peso} Kg`);
+let amigo = {nome:'José',
+sexo:'M',
+peso:85.4,
+engordar(p=0){
+   if(p > 0){
+    console.log('Engordou')
+   }else if(p < 0){
+    console.log('Emagreceu')
+   }else{
+    console.log('Não houve alterações')
+   }
+   this.peso += p
+}} //Declarando um objeto
+
+amigo.engordar(10)
+console.log(`Seu nome é ${amigo.nome} e pesa ${amigo.peso} Kg`)
